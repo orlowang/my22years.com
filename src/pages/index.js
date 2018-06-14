e8718b68e10ef9f0f5fa50eb1bd2f875a73e435d
orlo wang <ow.cc@outlook.com>
2018-06-14 17:30:53 +0800

    test netlify hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to my new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
